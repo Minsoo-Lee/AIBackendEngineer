409597743354dc5477d440b0a5d61c598e57e182
이민수 <minsoo.james1101@gmail.com>
2026-07-22 23:13:50 +0900

    Week2 - Day3

---
+++ src/main/java/roadmap/springai/controller/MyController.java
@@ -0,0 +1,7 @@
+package roadmap.springai.controller;
+
+import org.springframework.ai.chat.
+
+public class MyController {
+    private final ChatClient chatClient;
+}
---
+++ src/main/java/roadmap/springai/service/DocumentIngestionService.java
@@ -0,0 +1,4 @@
+package roadmap.springai.service;
+
+public class DocumentIngestionService {
+}
